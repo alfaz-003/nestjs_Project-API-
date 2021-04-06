b5cce6b22550e8e503441f7eda4e35ad8b41bc25
Alfaz Vahora <65657948+alfaz-003@users.noreply.github.com>
2021-04-06 15:22:23 +0530

    Update mongo-db.module.ts

--- src/mongo-db/mongo-db.module.ts
+++ src/mongo-db/mongo-db.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb+srv://nest:alfaz@123@cluster0.ylkxn.mongodb.net/sales&inventory?retryWrites=true&w=majority')]
+    imports: [MongooseModule.forRoot('mongodb+srv://nest:<password>@cluster0.ylkxn.mongodb.net/sales&inventory?retryWrites=true&w=majority')]
 })
 export class MongoDbModule {}
